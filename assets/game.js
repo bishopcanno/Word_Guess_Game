7caff1b8bcaff44f7c74e2ececa4e9d02332e1b3
bishopcanno <ericbispres@gmail.com>
2018-08-11 11:47:28 -0600

    added DRY elements to JS

--- assets/game.js
+++ assets/game.js
@@ -1,50 +1,49 @@
-// function genCharArray(charA, charZ) {
-    // var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
-    // for (; i <= j; ++i) {
-        // a.push(String.fromCharCode(i));
-    // }
-    // return a;
-// }
-// genCharArray('a', 'z');
-// a way to write an alphabet array as a funtion, dont fully understand how it works yet and thus dont want to use it, ask instrs.
+function genCharArray(charA, charZ) {
+    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return a;
+}
+var compChoice = genCharArray('a', 'z');
+  
 
-var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+// var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 var wins = 0;
 var losses = 0;
 var guesses = 5;
-document.onkeyup = function(event) {
+document.onkeypress = function(event) {
 
     var userGuess = event.key.toLowerCase();
     
     var compGuess = compChoice[Math.floor(Math.random() * compChoice.length)];
-    //perhaps use a for loop to solve this convoution?
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g")
-        || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n")
-        || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u")
-        || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
-            
+    
+    if ((userGuess.charCodeAt(0) >= 97 && userGuess.charCodeAt(0) <= 122)){
+        //certain ancilary keys are giving me viable number e.g. capslock returns a value of 99, which is valid because the string capslock at index 0 is 99 or c, but y? use onkeypress
+        
+        console.log(userGuess.charCodeAt(0));
         //   console.log(userGuess);
         //   console.log(compGuess);
-          // code good through here
         if (userGuess === compGuess){
             wins++;
             guesses = 5;
 
-        }
-        // code good through here
-        else if (userGuess !== compGuess){
+        } else if (userGuess !== compGuess){
             guesses--;
                 if (guesses === 0){
                     losses++;
                     guesses = 5;
                 }
                 
-        }
+        } 
+        
         var htmlScore =
         "<h3>Wins: " + wins + "</h3>" +
         "<h3>Losses: " + losses + "</h3>" +
         "<h3>Guesses left: " + guesses + "</h3>" +
         "<h3>Last letter selected: " + userGuess + "</h3>";
+        // need to figure out how to make it display all the letters the user inputs
         
       
         document.querySelector("#scoreBoard").innerHTML = htmlScore;
